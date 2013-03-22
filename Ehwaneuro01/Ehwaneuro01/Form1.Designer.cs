f1466777353dd4b4c47b0f754f97532e9fd03439
Kais_DkM <kais_dm@naver.com>
2013-03-22 17:23:35 +0900

    2013-03-22
    시리얼통신 부분 new사용 직전
    이벤트 핸들러 추가 해야 함

--- Ehwaneuro01/Ehwaneuro01/Form1.Designer.cs
+++ Ehwaneuro01/Ehwaneuro01/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PortNames = new System.Windows.Forms.ComboBox();
+            this.BaudRates = new System.Windows.Forms.ComboBox();
+            this.Conn_But = new System.Windows.Forms.Button();
+            this.Discon_But = new System.Windows.Forms.Button();
+            this.Sendtxt = new System.Windows.Forms.TextBox();
+            this.Recievtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PortNames
+            // 
+            this.PortNames.FormattingEnabled = true;
+            this.PortNames.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11"});
+            this.PortNames.Location = new System.Drawing.Point(8, 10);
+            this.PortNames.Name = "PortNames";
+            this.PortNames.Size = new System.Drawing.Size(85, 20);
+            this.PortNames.TabIndex = 0;
+            // 
+            // BaudRates
+            // 
+            this.BaudRates.FormattingEnabled = true;
+            this.BaudRates.Items.AddRange(new object[] {
+            "9600",
+            "28800",
+            "57600",
+            "76800",
+            "115200"});
+            this.BaudRates.Location = new System.Drawing.Point(8, 36);
+            this.BaudRates.Name = "BaudRates";
+            this.BaudRates.Size = new System.Drawing.Size(85, 20);
+            this.BaudRates.TabIndex = 1;
+            // 
+            // Conn_But
+            // 
+            this.Conn_But.Location = new System.Drawing.Point(8, 62);
+            this.Conn_But.Name = "Conn_But";
+            this.Conn_But.Size = new System.Drawing.Size(85, 20);
+            this.Conn_But.TabIndex = 2;
+            this.Conn_But.Text = "Connect";
+            this.Conn_But.UseVisualStyleBackColor = true;
+            this.Conn_But.Click += new System.EventHandler(this.Conn_But_Click);
+            // 
+            // Discon_But
+            // 
+            this.Discon_But.Location = new System.Drawing.Point(8, 88);
+            this.Discon_But.Name = "Discon_But";
+            this.Discon_But.Size = new System.Drawing.Size(85, 20);
+            this.Discon_But.TabIndex = 3;
+            this.Discon_But.Text = "Disconnect";
+            this.Discon_But.UseVisualStyleBackColor = true;
+            this.Discon_But.Click += new System.EventHandler(this.Discon_But_Click);
+            // 
+            // Sendtxt
+            // 
+            this.Sendtxt.Location = new System.Drawing.Point(99, 10);
+            this.Sendtxt.Multiline = true;
+            this.Sendtxt.Name = "Sendtxt";
+            this.Sendtxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Sendtxt.Size = new System.Drawing.Size(175, 98);
+            this.Sendtxt.TabIndex = 4;
+            this.Sendtxt.TextChanged += new System.EventHandler(this.Sendtxt_TextChanged);
+            // 
+            // Recievtxt
+            // 
+            this.Recievtxt.Location = new System.Drawing.Point(8, 114);
+            this.Recievtxt.Multiline = true;
+            this.Recievtxt.Name = "Recievtxt";
+            this.Recievtxt.ReadOnly = true;
+            this.Recievtxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Recievtxt.Size = new System.Drawing.Size(264, 136);
+            this.Recievtxt.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Recievtxt);
+            this.Controls.Add(this.Sendtxt);
+            this.Controls.Add(this.Discon_But);
+            this.Controls.Add(this.Conn_But);
+            this.Controls.Add(this.BaudRates);
+            this.Controls.Add(this.PortNames);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox PortNames;
+        private System.Windows.Forms.ComboBox BaudRates;
+        private System.Windows.Forms.Button Conn_But;
+        private System.Windows.Forms.Button Discon_But;
+        private System.Windows.Forms.TextBox Sendtxt;
+        private System.Windows.Forms.TextBox Recievtxt;
     }
 }
 
--- Ehwaneuro01/Ehwaneuro01/Form1.cs
+++ Ehwaneuro01/Ehwaneuro01/Form1.cs
@@ -6,14 +6,38 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Ehwaneuro01
 {
     public partial class Form1 : Form
     {
+        SerialPort serialP;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Conn_But_Click(object sender, EventArgs e)
+        {
+            if (!serialP.IsOpen)
+            {
+                serialP.PortName = PortNames.SelectedText;
+                serialP.BaudRate = Convert.ToInt32(BaudRates.SelectedText);
+
+                serialP.Open();
+            }
+        }
+
+        private void Discon_But_Click(object sender, EventArgs e)
+        {
+            if (serialP.IsOpen) serialP.Close();
+        }
+
+        private void Sendtxt_TextChanged(object sender, EventArgs e)
+        {
+            serialP.WriteLine(Sendtxt.SelectedText);
         }
     }
 }
